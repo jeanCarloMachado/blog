0d047c1a666d974690a3c4921db148bed03633d1
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2015-04-12 19:29:04 -0300

    paginação implementada

--- module/SiteJean/src/SiteJean/Controller/IndexController.php
+++ module/SiteJean/src/SiteJean/Controller/IndexController.php
@@ -1,17 +1,37 @@
 <?php
 namespace SiteJean\Controller;
+
 use AckMvc\Controller\Base;
+
 class IndexController extends Base
 {
+    const POSTS_PER_PAGE = 1;
+    private $currentPage = 0;
+
     public function indexAction()
     {
         $variables = array();
 
-        $variables['posts'] = $this->getServiceLocator()
-            ->get('Posts')
-            ->toObject()
-            ->get(array('publicado'=>1),array('order'=> 'data DESC'));
+        if (isset($_GET['page']) && is_int((int) $_GET['page'])) {
+            $this->currentPage = abs($_GET['page']);
+        }
 
+        $postsService = $this->getServiceLocator() ->get('Posts');
+        $totalPosts =  $postsService->count(array('publicado' => 1));
+
+        $variables['posts'] = $postsService
+            ->toObject()
+            ->get(
+                array('publicado'=>1),
+                array(
+                    'order'=> 'data DESC',
+                    'limit' => [ 'offset'=>$this->currentPage * self::POSTS_PER_PAGE, 'count'=> self::POSTS_PER_PAGE]
+                )
+            );
+
+        $variables['currentPage'] = $this->currentPage;
+        $variables['totalPosts'] = $totalPosts;
+        $variables['totalPages'] = $totalPosts / self::POSTS_PER_PAGE;
         $this->viewModel->setVariables($variables);
 
         return $this->viewModel;
