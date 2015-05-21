2a8f43c647c4085056b565fb65e589904f56a0f2
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2015-05-21 09:01:15 -0300

    able to render a post

--- frontend/main.js
+++ frontend/main.js
@@ -30,10 +30,13 @@ document.getElementById("link-about").onclick = function() {
     loadViewPort('about');
 };
 
-document.getElementById("link-posts").onclick = function() {
-    hideAllViewPorts();
-    loadViewPort('posts');
-};
+elements = document.getElementsByClassName("link-posts")
+for (i = 0; i < elements.length; i++) {
+    elements[i].onclick = function() {
+        hideAllViewPorts();
+        loadViewPort('posts');
+    };
+}
 
 document.getElementById("link-feed").href = config.backendUrl + '/feed';
 
@@ -42,7 +45,6 @@ window.onload = function () {
     hideAllViewPorts();
     loadViewPort('posts');
 }
-
 
 
 addEventListener('load-posts', function (e) {
@@ -59,18 +61,55 @@ addEventListener('load-posts', function (e) {
         var article = document.createElement('article');
         var header = document.createElement('header');
         var h1 = document.createElement('h1');
+        var a = document.createElement('a');
         var title = document.createTextNode(posts[i].titulo);
         var date = document.createTextNode(posts[i].data);
         var div = document.createElement('div');
         var content = document.createTextNode(posts[i].conteudo);
 
+        a.class = 'link-posts';
+        a.id = posts[i].id;
+
+        a.onclick = function() {
+            hideAllViewPorts();
+            window.currentPost = this.id;
+            loadViewPort('post');
+        };
+
         header.appendChild(h1);
         header.appendChild(date);
-        h1.appendChild(title);
+        h1.appendChild(a);
+        a.appendChild(title);
         article.appendChild(header);
         article.appendChild(div);
         div.appendChild(content);
         li.appendChild(article);
         postsList.appendChild(li);
     }
+});
+
+
+addEventListener('load-post', function (e) {
+    xmlhttp= new XMLHttpRequest();
+    xmlhttp.open("POST", config.backendUrl+"/post/json/"+window.currentPost, false);
+    xmlhttp.send();
+    data = JSON.parse(xmlhttp.responseText);
+    var title = document.createTextNode(data.row.vars.titulo.bruteValue);
+    var content = document.createTextNode(data.row.vars.conteudo.bruteValue);
+    var date = document.createTextNode(data.row.vars.data.bruteValue);
+
+    var postViewPort = document.getElementById("post");
+    var article = postViewPort.children[0];
+    console.log(article);
+    var header = document.createElement('header');
+    var h1 = document.createElement('h1');
+    var div = document.createElement('div');
+
+    h1.appendChild(title);
+    header.appendChild(h1);
+    header.appendChild(date);
+    div.appendChild(content);
+    article.html = '';
+    article.appendChild(header);
+    article.appendChild(div);
 });
