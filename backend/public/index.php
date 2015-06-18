b454c7f658ea75387c4a3407363274855f4e2b5f
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2015-06-18 08:07:46 -0300

    [skip ci] added md converter by extension

--- backend/public/index.php
+++ backend/public/index.php
@@ -4,12 +4,12 @@ use Zend\Stratigility\MiddlewarePipe;
 use Zend\Diactoros\Server;
 use Blog\Service\Post;
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 $files = glob('../config/{global,local}*.php', GLOB_BRACE);
 $config = Zend\Config\Factory::fromFiles($files);
 
-$app    = new MiddlewarePipe();
+$app = new MiddlewarePipe();
 $server = Server::createServer($app, $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
 
 $adapter = new Zend\Db\Adapter\Adapter($config['database']);
@@ -36,46 +36,30 @@ $app->pipe('/posts', function ($req, $res, $next) use ($adapter) {
         $result = $post->getOnlyResumeOfContent($result);
     }
 
-    $result = json_encode($result, true);
-    $res->write($result);
 
-    if ($req->getUri()->getPath() !== '/posts') {
-        $next($req, $res);
-    }
+    if (preg_match('/md$/', $req->getUri()->getPath())) {
 
-    return $res->end();
-});
+        $parsedown = new Parsedown;
+        $textToMarkdown = function ($data) use ($parsedown, &$textToMarkdown) {
+            if (is_array($data)) {
+                foreach ($data as $key => $entry) {
+                    if ($key == 'conteudo')
+                        $result[$key] = $textToMarkdown($entry);
+                    else
+                        $result[$key] = $entry;
+                }
 
-$app->pipe('/', function ($req, $res, $next) {
-
-    if (!preg_match("/md$/", $req->getUri()->getPath())) {
-        return $next($req, $res);
-    }
-
-    $data = $res->getBody();
-
-
-    if (is_array(json_decode($data, true))) {
-        $data = json_decode($data, true);
-    }
-
-    $parsedown = new Parsedown();
-
-    $textToMarkdown = function($data) use ($parsedown, &$textToMarkDown) {
-        if (is_array($data)) {
-            foreach ($data as $key => $entry) {
-                $result[$key] = $textToMarkDown($entry);
+                return $result;
             }
-        }
 
-        $result = $parsedown->text($data);
-    };
+            return $parsedown->text($data);
+        };
 
-    $result = $textToMarkDown($data);
+        $result = $textToMarkdown($result);
+    }
 
-    return $res->end(json_encode($result, true));
+    $result = json_encode($result, true);
+    return $res->end($result);
 });
-
-
 
 $server->listen();
